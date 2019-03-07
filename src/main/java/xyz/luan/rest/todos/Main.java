00d28dc69d4ed85a9a637032a45e83cbaa219a15
Luan Nico <luanpotter27@gmail.com>
2019-03-07 00:31:08 -0300

    Adding spark java

--- src/main/java/xyz/luan/rest/todos/Main.java
+++ src/main/java/xyz/luan/rest/todos/Main.java
@@ -1,7 +1,9 @@
 package xyz.luan.rest.todos;
 
+import static spark.Spark.*;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
 }
